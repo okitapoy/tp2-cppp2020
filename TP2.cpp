8cdaa8df82d74f5526a8294e260120ab19d9982c
okitapoy <okitapoy@gmail.com>
2020-07-22 23:06:32 -0400

    dd

--- TP2.cpp
+++ TP2.cpp
@@ -73,52 +73,61 @@ int main() {
 
 //ArbreMap<string, int> *mapTest = new ArbreMap<string, int>[histoires->size()];
 
-ArbreMapAVL<string,int> avl;
+//ArbreMapAVL<string,int> *avl = new ArbreMapAVL<string,int>[histoires->size()];
 
-avl.inserer("un",11);
-avl.inserer("deux",22);
-avl.inserer("trois",33);
-avl.inserer("quatre",44);
+//avl.inserer("un",11);
+//avl.inserer("deux",22);
+//avl.inserer("trois",33);
+//avl.inserer("quatre",44);
 
-//ArbreAVL<int>::Iterateur iter;
+//ArbreAVL<int>::Iterateur iter;iterer
 //ArbreMap<string, int>::Iterateur iter(avl);// = avl.rechercher(1);
 
-ArbreMapAVL<string, int>::Iterateur iterer = avl.debut();
+//ArbreMapAVL<string, int>::Iterateur iterer = avl.debut();
 
-ArbreMapAVL<string, int>::Iterateur it = avl.rechercher("un");
+//ArbreMapAVL<string, int>::Iterateur it = avl.rechercher("un");
 //string cle = avl.operator[](iterer);
 
-if(it){
+//if(it){
 //  cout << "iter a fonctionnner" << endl;
-}
+//}
 //cout << avl.operator[](cle) << endl;
 
 
 
-if(avl.contient("trois")){
-  Noeud noeud("trois",3333);
-  cout << avl.operator[](cle) << endl;
-}
+//if(avl.contient("trois")){
+//  Noeud noeud("trois",3333);
+//cout << avl.operator[]("trois") << endl;
+//avl.inserer("trois",3333339);
+//}
 
 
 
 
 
-    exit(-1);
+
+  //  exit(-1);
 
 
 
 
-     vector<string>::const_iterator iterPhrase;
+  //   vector<string>::const_iterator iterPhrase;
+  ArbreMapAVL<string,int> *avl = new ArbreMapAVL<string,int>[histoires->size()];
+  int tempChiffre = 0;
+  int index = 0;
 
+cout << "size : " <<  histoires->size() << endl;
     // Pour votre projet, enlevez le code qui suit et remplacer le par votre code.
-    // vous pouvez ajouter des fonctions.
+    // vous pouvez ajouter des fonctionions.
 
     // Parcourir les histoires a l'aide de l'iterateur du vecteur.
     for( Histoire * histoire : * histoires )
     {
         // Les histoires ont une variable de classe 'titre'.
-        cout << histoire->titre() << endl;
+        //cout << histoire->titre() << endl;
+        cout << "index  : " << index << endl;
+        avl[index] = ArbreMapAVL<string,int>();
+        cout << "rien" << endl;
 
         // Parcourir les Phrases qui compose une histoire � l'aide de l'iterateur des Histoires.
         for( Phrase p : * histoire )
@@ -128,16 +137,53 @@ if(avl.contient("trois")){
             // ici, nous affichons seulement le premier mot de la Phrase.
           //  cout< *( p.begin() ) << endl;
 
-          for(iterPhrase = p.begin(); iterPhrase != p.end(); ++iterPhrase){
+
+
+          for(auto iterPhrase = p.begin(); iterPhrase != p.end(); ++iterPhrase){
             string mot =  *iterPhrase;
-          //  cout << mot << endl;
-          }
+
+
+            if(avl[index].contient(mot)){
+              tempChiffre =  avl[index].operator[](mot) + 1;
+              avl[index].inserer(mot,tempChiffre);
+
+            }else{
+              avl[index].inserer(mot,1);
+            }
+
+         }
 
         }
 
-        cout << endl;
-        break;
+        //cout << endl;
+        //break;
+        ++index;
     }
 
+
+//test affiche arbre
+//ArbreMapAVL<string, int>::Iterateur iterer = avl.debut();
+//cout << avl.operator[](iterer) << endl;
+
+//ArbreMapAVL<string, int>::Iterateur it = avl.rechercher("a");
+int cle = avl[25].operator[]("is");
+cout << cle << endl;
+
+/*
+for(auto iterer = avl.debut(); iterer != avl.fin(); ++iterer){
+  string cle = avl.operator[](iterer);
+  int nombre = avl.operator[](cle);
+
+  cout << cle << " : " << nombre << endl;
+
+}*/
+
+
     return 0;
+
+
+
+    /*the hideous monster
+    launched itself into the air straight toward them. (_To be concluded in the February
+    Number._) </chapitre> */
 }
